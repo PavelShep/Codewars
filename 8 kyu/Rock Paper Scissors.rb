a68c6ecd9e22fd5edab85bf68dd67b0b98095dd2
Litwin_z_Grodno <71721882+PavelShep@users.noreply.github.com>
2023-05-27 22:12:02 +0200

    Update Rock Paper Scissors.rb

--- 8 kyu/Rock Paper Scissors.rb
+++ 8 kyu/Rock Paper Scissors.rb
@@ -12,12 +12,11 @@ My solution
 =end
 
 def rps(p1, p2)
- if p1 == p2
-  return "Draw!"
-  elsif
-  (p1 == "rock" && p2 == "scissors") || (p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock")
-  return "Player 1 won!"
+  if p1 == p2
+    return "Draw!"
+  elsif (p1 == "rock" && p2 == "scissors") || (p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock")
+    return "Player 1 won!"
   else
-  return "Player 2 won!"
-end
+    return "Player 2 won!"
+  end
 end
